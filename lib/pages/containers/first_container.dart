4034e84c79878e5075ad84f512c88467eed163ed
Ashutosh <am3718440@gmail.com>
2023-12-25 22:02:14 +0530

    EXPENSE: Added Tablet mode for first screen
    Signed-off-by: Ashutosh <am3718440@gmail.com>

--- lib/pages/containers/first_container.dart
+++ lib/pages/containers/first_container.dart
@@ -17,6 +17,7 @@ class _FirstContainerState extends State<FirstContainer> {
     return ScreenTypeLayout(
       mobile: MobileFirstContainer(),
       desktop: DesktopFirstContainer(),
+      tablet: TabletFirstContainer(),
     );
   }
   Widget MobileFirstContainer(){
@@ -25,8 +26,8 @@ class _FirstContainerState extends State<FirstContainer> {
       child: Column(
         children: [
           Container(
-            height: MediaQuery.sizeOf(context).width/1.2,
-            width: MediaQuery.sizeOf(context).width/1.2,
+            height: MediaQuery.sizeOf(context).width/1.5,
+            width: MediaQuery.sizeOf(context).width/1.5,
             decoration: BoxDecoration(
               image: DecorationImage(image:AssetImage(illustrration_1),
                   fit: BoxFit.contain),
@@ -35,32 +36,32 @@ class _FirstContainerState extends State<FirstContainer> {
           SizedBox(height: 20,),
           Container(
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Text("Track your \nExpenses to \nSave Money", style: TextStyle(
+                Text("Track your Expenses to Save Money", style: TextStyle(
                     fontSize: MediaQuery.sizeOf(context).width/20,
                     fontWeight: FontWeight.bold,
                     height: 1.2
-                ),),
+                ), textAlign: TextAlign.center,),
                 Text("Helps you to organize your income and expenses", style: TextStyle(
                   fontWeight: FontWeight.w400,
-                  fontSize: 20,
+                  fontSize: MediaQuery.sizeOf(context).width/30,
                   color: Color(0xffBDBDBD),
                 ),),
                 SizedBox(height: 30,),
                 Container(
-                  height: 45,
+                  height: 35,
                   child: ElevatedButton.icon(
                     style: ButtonStyle(
                         backgroundColor: MaterialStateProperty.all(MyColors.primaryColor)
                     ),
                     onPressed: (){},
                     icon: Icon(Icons.arrow_drop_down),
-                    label: Text("Try free demo", style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w400),),
+                    label: Text("Try free demo", style: TextStyle(color: Colors.white, fontSize: MediaQuery.sizeOf(context).width/40, fontWeight: FontWeight.w400),),
                   ),
                 ),
-                SizedBox(width: 20,),
-                Text("— Web, iOs and Android", style: TextStyle(color: Color(0xffBDBDBD), fontSize: 20),)
+                SizedBox(width: 20, height: 10,),
+                Text("— Web, iOs and Android", style: TextStyle(color: Color(0xffBDBDBD), fontSize: MediaQuery.sizeOf(context).width/30),)
               ],
             ),
           ),
@@ -130,4 +131,55 @@ class _FirstContainerState extends State<FirstContainer> {
       ),
     );
   }
+
+  Widget TabletFirstContainer(){
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 20, horizontal: MediaQuery.sizeOf(context).width/10),
+      child: Column(
+        children: [
+          Container(
+            height: MediaQuery.sizeOf(context).width/1.2,
+            width: MediaQuery.sizeOf(context).width/1.2,
+            decoration: BoxDecoration(
+              image: DecorationImage(image:AssetImage(illustrration_1),
+                  fit: BoxFit.contain),
+            ),
+          ),
+          SizedBox(height: 20,),
+          Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Track your Expenses to \nSave Money", style: TextStyle(
+                    fontSize: MediaQuery.sizeOf(context).width/20,
+                    fontWeight: FontWeight.bold,
+                    height: 1.2
+                ),),
+                Text("Helps you to organize your income and expenses", style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20,
+                  color: Color(0xffBDBDBD),
+                ),),
+                SizedBox(height: 30,),
+                Container(
+                  height: 45,
+                  child: ElevatedButton.icon(
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(MyColors.primaryColor)
+                    ),
+                    onPressed: (){},
+                    icon: Icon(Icons.arrow_drop_down),
+                    label: Text("Try free demo", style: TextStyle(color: Colors.white, fontSize: MediaQuery.sizeOf(context).width/40, fontWeight: FontWeight.w400),),
+                  ),
+                ),
+                SizedBox(width: 20, height: 10,),
+                Text("— Web, iOs and Android", style: TextStyle(color: Color(0xffBDBDBD), fontSize: 20),)
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
 }
