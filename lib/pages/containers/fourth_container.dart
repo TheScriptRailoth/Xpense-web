605c79274646f0952d7d9f4c946e6d25fa02b046
Ashutosh <am3718440@gmail.com>
2023-12-26 22:35:13 +0530

    XPENSE: Added fourth screen for mobile screen
    Signed-off-by: Ashutosh <am3718440@gmail.com>

--- lib/pages/containers/fourth_container.dart
+++ lib/pages/containers/fourth_container.dart
@@ -61,7 +61,35 @@ class _FourthContainerState extends State<FourthContainer> {
   }
 
   Widget MobileFourthContainer(){
-    return Container();
+    return Container(
+      color: Colors.white,
+      padding: EdgeInsets.symmetric(horizontal: MediaQuery.sizeOf(context).width/10, vertical: 50),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            height: 300,
+            child: Image.asset(illustration_3, fit: BoxFit.contain,),
+          ),
+          SizedBox(height: 30,),
+          Text("FREE SOME COST", style: TextStyle(color:Color(0xffBDBDBD), fontSize: 20, fontWeight: FontWeight.w400)),
+          SizedBox(height: 10,),
+          Text("Save cost for you and family", style: TextStyle(fontWeight: FontWeight.bold, fontSize: MediaQuery.sizeOf(context).width/20, height: 1.2),),
+          SizedBox(height: 10,),
+          Text("Tellus lacus morbi sagittis lacus in. Amet nisl at \nmauris enim accumsan nisi, tincidunt vel. Enim \nipsum, amet quis ullamcorper eget ut.", style: TextStyle(
+            color: Color(0xffBDBDBD), fontSize: MediaQuery.sizeOf(context).width/30,
+          ),),
+          SizedBox(height: 10,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Learn More", style: TextStyle(color:MyColors.primaryColor, fontSize: MediaQuery.sizeOf(context).width/30, fontWeight: FontWeight.w400)),
+              Icon(Icons.arrow_forward_rounded, color: MyColors.primaryColor,)
+            ],
+          )
+        ],
+      ),
+    );
   }
 
   Widget TabletFourthContainer(){
