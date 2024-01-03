c16f10d17c49faadbcc590ad95bf10695486aa9f
Ashutosh <am3718440@gmail.com>
2024-01-03 23:53:32 +0530

    XPENSE-WEB: Added info end screen for mobile mode
    Signed-off-by: Ashutosh <am3718440@gmail.com>

--- lib/pages/containers/eighth_container.dart
+++ lib/pages/containers/eighth_container.dart
@@ -536,6 +536,270 @@ class _EightContainerState extends State<EightContainer> {
   }
 
   Widget MobileEightContainer(){
-    return Container();
+    return Container(
+      padding: EdgeInsets.only(left: 50,right: 50, top: 100, bottom: 30),
+      // height: 530,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                              height: 40,
+                              child: Image.asset('assets/Logo.png')
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text("LINKS", style: TextStyle(color: Colors.black, fontSize: MediaQuery.sizeOf(context).width/30),),
+                          SizedBox(height: 20,),
+                          InkWell(
+                            onTap: () {},
+                            child: Text(
+                              'Home',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: MediaQuery.sizeOf(context).width/50,
+                              ),
+                            ),
+                          ),
+                          InkWell(
+                            onTap: () {},
+                            child: Text(
+                              'About Us',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: MediaQuery.sizeOf(context).width/50,
+                              ),
+                            ),
+                          ),
+                          InkWell(
+                            onTap: () {},
+                            child: Text(
+                              'Career',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: MediaQuery.sizeOf(context).width/50,
+                              ),
+                            ),
+                          ),
+                          InkWell(
+                            onTap: () {},
+                            child: Text(
+                              'Pricing',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: MediaQuery.sizeOf(context).width/50,
+                              ),
+                            ),
+                          ),
+                          InkWell(
+                            onTap: () {},
+                            child: Text(
+                              'Feature',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: MediaQuery.sizeOf(context).width/50,
+                              ),
+                            ),
+                          ),
+                          InkWell(
+                            onTap: () {},
+                            child: Text(
+                              'Blogs',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: MediaQuery.sizeOf(context).width/50,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 30,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("LEGAL", style: TextStyle(color: Colors.black, fontSize: MediaQuery.sizeOf(context).width/30),),
+                          SizedBox(height: 20,),
+                          InkWell(
+                            onTap: () {},
+                            child: Text(
+                              'Terms of use',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: MediaQuery.sizeOf(context).width/50,
+                              ),
+                            ),
+                          ),
+                          InkWell(
+                            onTap: () {},
+                            child: Text(
+                              'Terms of condition',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: MediaQuery.sizeOf(context).width/50,
+                              ),
+                            ),
+                          ),
+                          InkWell(
+                            onTap: () {},
+                            child: Text(
+                              'Privacy condition',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: MediaQuery.sizeOf(context).width/50,
+                              ),
+                            ),
+                          ),
+                          InkWell(
+                            onTap: () {},
+                            child: Text(
+                              'Cookie policy',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: MediaQuery.sizeOf(context).width/50,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("NEWSLETTER", style: TextStyle(color: Colors.black, fontSize: MediaQuery.sizeOf(context).width/30),),
+                          SizedBox(height: 20,),
+                          Text("Over 25000 people have subscribed", style: TextStyle(
+                            color: Color(0xffBDBDBD),
+                            fontSize: MediaQuery.sizeOf(context).width/50,
+                          ),),
+                          SizedBox(height: 20,),
+                          Container(
+                            height: MediaQuery.sizeOf(context).width*0.06,
+                            width: MediaQuery.sizeOf(context).width*0.35,
+                            decoration: BoxDecoration(
+                                border: Border.all(color: Colors.black, width: 1),
+                                borderRadius: BorderRadius.circular(10)
+                            ),
+                            child: Row(
+                              children: [
+                                SizedBox(width: 10,),
+                                Expanded(
+                                    child: Center(
+                                      child: TextFormField(
+                                        decoration: InputDecoration(
+                                          hintText: "Enter Your Email",
+                                          border: InputBorder.none,
+                                        ),
+                                        style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: MediaQuery.sizeOf(context).width/60
+                                        ),
+                                        cursorColor: MyColors.primaryColor,
+                                      ),
+                                    )
+                                ),
+                                SizedBox(width: 10,),
+                                Container(
+                                  height: MediaQuery.sizeOf(context).width*0.048,
+                                  width: MediaQuery.sizeOf(context).width*0.098,
+                                  decoration: BoxDecoration(
+                                      color: MyColors.primaryColor,
+                                      borderRadius: BorderRadius.circular(10)
+                                  ),
+                                  child: ElevatedButton(
+                                    style: ButtonStyle(
+                                        backgroundColor: MaterialStateProperty.all(MyColors.primaryColor)
+                                    ),
+                                    onPressed: (){},
+                                    child: Text(
+                                      "Subscribe",
+                                      style: TextStyle(color: Colors.white, fontSize: MediaQuery.sizeOf(context).width/70),
+                                      overflow: TextOverflow.ellipsis,
+                                      maxLines: 1,
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(width: 10,)
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: 20,),
+                          Text("We don’t sell your email and spam", style: TextStyle(
+                            color: Color(0xffBDBDBD),
+                            fontSize: MediaQuery.sizeOf(context).width/60,
+                          ),),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 40,),
+          Container(
+            height: 2,
+            width: MediaQuery.sizeOf(context).width,
+            color: Colors.grey.withOpacity(0.2),
+          ),
+          SizedBox(height: 40,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  TextButton(
+                    onPressed: (){},
+                    child: Text("Privacy & Terms", style: TextStyle(
+                      color: Colors.black,
+                      fontSize: MediaQuery.sizeOf(context).width/60,
+                    ),),
+                  ),
+                  SizedBox(width: 30,),
+                  TextButton(
+                    onPressed: (){},
+                    child: Text("Contact Us", style: TextStyle(
+                      color: Colors.black,
+                      fontSize: MediaQuery.sizeOf(context).width/60,
+                    ),),
+                  ),
+                ],
+              ),
+              Text("Copyright @ 2022 xpence", style: TextStyle(
+                color: Colors.black,
+                fontSize: MediaQuery.sizeOf(context).width/60,
+              ),),
+              Image.asset('assets/social_media_icon.png')
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
